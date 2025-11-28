a8965facfe470394d68215c98bc0ce4e09463820
kat <videomemes145@gmail.com>
2025-11-28 15:16:11 -0500

    Switched from Integers to Ints and added code to check all permutations of the input

--- 2024_02_01.hs
+++ 2024_02_01.hs
@@ -2,11 +2,11 @@ import Data.List
 import System.IO
 
 -- Create a list of integers from a string line
-lineToInts :: String -> [Integer]
+lineToInts :: String -> [Int]
 lineToInts line =
     map read $ words line
 
-diffList :: [Integer] -> [Integer]
+diffList :: [Int] -> [Int]
 diffList l = zipWith (-) (init l) (tail l)
 
 data ReactorDirection = Unknown
@@ -14,13 +14,13 @@ data ReactorDirection = Unknown
         | Decreasing
     deriving (Eq, Show)
 
-getState :: Integer -> ReactorDirection
+getState :: Int -> ReactorDirection
 getState change
     | change >  0 = Increasing
     | change <  0 = Decreasing
     | otherwise   = undefined
 
-checkState :: ReactorDirection -> [Integer] -> Bool
+checkState :: ReactorDirection -> [Int] -> Bool
 checkState direction differences
     | length differences == 0                           = True
     | isUnsafeDiff $ head differences                   = False
@@ -29,10 +29,27 @@ checkState direction differences
     | otherwise                                         = undefined
     where currentDir = getState $ head differences
 
-newCheckState :: [Integer] -> Bool
+newCheckState :: [Int] -> Bool
 newCheckState = checkState Unknown
 
-isUnsafeDiff :: Integer -> Bool
+checkAllRemovals :: [Int] -> Bool
+checkAllRemovals differences = or $ map newCheckState (newRemovalList differences)
+
+newRemovalList :: [Int] -> [[Int]]
+newRemovalList differences = removalList [] differences (length differences -1)
+
+removalList :: [[Int]] -> [Int] -> Int -> [[Int]]
+removalList perms differences index
+    | index == -1                   = perms
+    | length perms == 0             = removalList [differences] differences index
+    | otherwise                     = removalList (deleteNth differences index:perms) differences (index-1)
+
+deleteNth :: [Int]-> Int -> [Int]
+deleteNth differences index =
+  let (before, after) = splitAt index  differences
+  in before ++ drop 1 after
+
+isUnsafeDiff :: Int -> Bool
 isUnsafeDiff difference
     | difference >  3     = True
     | difference < -3     = True
@@ -48,6 +65,6 @@ main = do
     -- Find the integer list representation of each line
     let matrix      = map lineToInts x
     let diffmatrix  = map diffList matrix
-    let safetys     = map newCheckState diffmatrix
+    let safetys     = map checkAllRemovals diffmatrix
     let totalSafe   = sum $ map fromEnum safetys
     putStr $ show totalSafe
