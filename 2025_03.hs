df4b67e29e53fab65fd7f7884bac75215bae1ee7
kat <videomemes145@gmail.com>
2025-12-03 17:53:51 -0500

    Solving pt 1

--- 2025_03.hs
+++ 2025_03.hs
@@ -3,13 +3,26 @@ import System.IO
 
 maxJolt x = maximum $ sort x
 
+findIdx :: Char -> String -> Int
+findIdx x xs = case i of 
+    Just i -> i
+    Nothing -> error "Jolt value missing" 
+    where i = elemIndex x xs
+
+
+
 pt1 = do
     -- Reading from the file
     handle <- openFile "2025_03_input" ReadMode
     contents <- hGetContents handle
     let lins = lines contents
     
-    let maxFirstJolt = map (\x -> maxJolt $ init x) lins
+    let firstJolt    = map (\x -> maxJolt $ init x) lins
+    let firstJoltIdx = zipWith findIdx firstJolt lins
+    let secndOptions = zipWith drop (map (\x -> x+1) firstJoltIdx) lins
+    let secondJolt   = map maxJolt secndOptions
 
+    let joltage      = zipWith ((\x y -> read [x, y]) :: Char -> Char -> Int) firstJolt secondJolt
+    let totalJoltage = sum joltage
 
-    putStr $ show maxFirstJolt
+    putStr $ show totalJoltage
