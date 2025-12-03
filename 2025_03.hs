478bac7fddd1ccaf6cb62f0e60af7ff25a8b6179
kat <videomemes145@gmail.com>
2025-12-03 18:24:06 -0500

    Solved Pt2

--- 2025_03.hs
+++ 2025_03.hs
@@ -11,15 +11,32 @@ findIdx x xs = case i of
     Nothing -> error "Jolt value missing" 
     where i = elemIndex x xs
 
--- drops everything up to the first occurence of x
+-- drops everything up to and including the first occurence of x
 dropIdx :: Char -> String -> String
 dropIdx x xs = case i of 
     Just i -> drop (i+1) xs
     Nothing -> error "Jolt value missing" 
     where i = elemIndex x xs
 
+-- Finds the jolt options
+joltOptions :: Int -> String -> String
+joltOptions joltsLeft xs = drop (joltsLeft-1) (reverse xs)
 
+-- Finds the max of the options
+maxJoltHere :: Int -> String -> Char
+maxJoltHere n xs = maximum $ joltOptions n xs
 
+-- Finds the maximum combination of joltages
+maxJoltCombo :: String -> Int -> String -> Int
+maxJoltCombo xs n c = case n of
+    0 -> read c
+    _ -> maxJoltCombo newXs (n-1) (c++[maxJ])
+    where 
+        maxJ  = maxJoltHere n xs
+        newXs = dropIdx maxJ xs
+
+newMaxJoltCombo :: String -> Int
+newMaxJoltCombo xs = maxJoltCombo xs 12 ""
 
 
 pt1 = do
@@ -39,4 +56,11 @@ pt1 = do
     putStr $ show totalJoltage
 
 pt2 = do
-    Nothing
+    -- Reading from the file
+    handle <- openFile "2025_03_input" ReadMode
+    contents <- hGetContents handle
+    let lins = lines contents
+
+    let totalJoltage = sum $ map newMaxJoltCombo lins
+
+    putStr $ show totalJoltage
