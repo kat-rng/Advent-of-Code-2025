7dd7b782622193311d49587a8422e48e03512b5b
katpy <92893261+kat-rng@users.noreply.github.com>
2025-12-04 10:32:01 -0500

    Renamed file to allow it to be exposed as a module

--- F2025_04.hs
+++ F2025_04.hs
@@ -1,6 +1,12 @@
 import Data.List
 import System.IO
 
+charToInt :: Char -> Int
+charToInt c = case c of
+    '.' -> 0
+    '@' -> 1
+    _   -> error "Invalid character"
+
 pt1 = do
     -- Reading from the file
     handle <- openFile "2025_04_input" ReadMode
