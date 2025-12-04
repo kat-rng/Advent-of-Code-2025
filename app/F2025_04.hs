85ef4812bfa383aa9890bd344066d7f1926e0e1b
katpy <92893261+kat-rng@users.noreply.github.com>
2025-12-04 12:02:04 -0500

    changing name and formatting of stencil

--- app/F2025_04.hs
+++ app/F2025_04.hs
@@ -2,6 +2,7 @@ module F2025_04 (pt1, pt2) where
 
 import Prelude as P
 import Data.Massiv.Array as A
+import Data.Massiv.Array.Stencil as S
 import System.IO
 
 charToInt :: Char -> Int
@@ -15,12 +16,15 @@ inputToLists :: String -> Array U Ix2 Int
 inputToLists xs = A.fromLists' A.Seq intLists
     where intLists = P.map (P.map charToInt) (lines xs)
 
-average3x3Stencil :: Integral a => Stencil Ix2 a a
-3x3Stencil = makeStencil (Sz (3 :. 3)) (1 :. 1) $ get ->
-        (  get (-1 :. -1) + get (-1 :. 0) + get (-1 :. 1) +
-           get ( 0 :. -1) +                 get ( 0 :. 1) +
-           get ( 1 :. -1) + get ( 1 :. 0) + get ( 1 :. 1)   )
-{-# INLINE average3x3Stencil #-}
+sum3x3Stencil :: Integral a => Stencil Ix2 a a
+sum3x3Stencil = makeStencil (Sz (3 :. 3)) (1 :. 1) 
+    (\get ->
+         get (-1 :. -1) + get (-1 :. 0) + get (-1 :. 1)
+      +  get ( 0 :. -1) +                 get ( 0 :. 1)
+      +  get ( 1 :. -1) + get ( 1 :. 0) + get ( 1 :. 1)
+      
+    )
+{-# INLINE sum3x3Stencil #-}
 
 pt1 :: IO ()
 pt1 = do
